a896cd001be3bf4fa90ea16bfa78ed9e5e2a02ae
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-25 21:59:48 +0900

    アプリケーション初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KumonoMukou::Application.config.secret_key_base = 'd9994cde3634ebaf23259b24d4cbb81207f833f5fd3a6d718d34a615b9e43973567e4d9d07343ce8708f4faf1b90052831d89af507490780a10f646f4d259bee'
