3648c81ff722aea9cbd9144479220a73727cfd10
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-25 22:43:23 +0900

    places#index、showの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 KumonoMukou::Application.routes.draw do
+  get "places/index"
+  get "places/show"
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
