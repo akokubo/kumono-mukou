e0c7c69ebcea7bcea78914bb324fbafa1b7308bf
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-25 22:45:25 +0900

    ルートの修正
    static_pages/homeの削除、placesリソースを利用

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 KumonoMukou::Application.routes.draw do
-  get "places/index"
-  get "places/show"
-  get "static_pages/home"
+  resources :places, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
